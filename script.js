bfbcb22aec42f3c081e028c9f912684a0407c193
Anna Marlena <annamarlena310@gmail.com>
2023-04-04 17:10:17 -0500

    Starting from scratch and simplifying

--- script.js
+++ script.js
@@ -9,24 +9,48 @@
 // Preferable not to have an event listener for each of the buttons
   // Have a global event listener for any click on the page
 
-  var timer
-  var score = 0;
-  
-// User arrives at web page
-// Click the button
+// Set the global variables
+var timer = 5;
+var score = 0;
+var startBtn = document.querySelector("#start-button");
+var questionScrn = document.querySelector("#question-screen");
+var welcomeScrn = document.querySelector("#welcome-screen");
+var gameoverScrn = document.querySelector("#gameover-screen");
+var nextBtn = document.querySelector("#next-button");
 
-function start() {
-
-  
+// Create a function that runs when a user starts the quiz
+function startQuiz() {
+  // Hide the welcome screen and show the first question
+  questionScrn.classList.remove("hide");
+  welcomeScrn.classList.add("hide");
 }
+
+// Create a function that runs when a user clicks the next button
+function nextScreen() {
+  // Hide the current question and go to the next screen
+  questionScrn.classList.add("hide");
+  gameoverScrn.classList.remove("hide");
+}
+
   // First question appears
   // Timer starts
+  // function countdown() {
+
+  // }
 
   // Listen for user to click on one of the answers
 
 // When an answer is clicked
   // Determine if it's right or wrong
-
+// function checkAnswer() 
     // If it's right
 
     // If it's wrong
+
+
+// Listen for button clicks
+startBtn.addEventListener("click", startQuiz)
+nextBtn.addEventListener("click", nextScreen)
+
+
+}
