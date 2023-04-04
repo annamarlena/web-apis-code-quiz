414a11796943876a9902e3f2480e6aab4452bb81
Anna Marlena <annamarlena310@gmail.com>
2023-04-04 08:20:15 -0500

    Created files, wrote pseudocode, created the bones of the html and css

---
+++ script.js
@@ -0,0 +1,24 @@
+// Gary's Tips:
+// Need global vars
+// Need functions
+// Need a way to cycle through all the answers and store them
+// Start off by making 2 generic questions and run tests till working right
+// Test in short, controlled areas, can always make everything bigger later on
+// Don't make timer really long
+// Test functions in isolation: write the function, call the function to test it
+// Preferable not to have an event listener for each of the buttons
+  // Have a global event listener for any click on the page
+
+// User arrives at web page
+// Click the button
+  // First question appears
+  // Timer starts
+
+  // Listen for user to click on one of the answers
+
+// When an answer is clicked
+  // Determine if it's right or wrong
+
+    // If it's right
+
+    // If it's wrong
